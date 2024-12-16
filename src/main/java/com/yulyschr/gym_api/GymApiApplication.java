aadd759f845049e15dcbd35c73830a379f8b62aa
yulychr <sandra_ch.r@hotmail.com>
2024-12-16 11:25:07 -0500

    chore: initial project setup

---
+++ src/main/java/com/yulyschr/gym_api/GymApiApplication.java
@@ -0,0 +1,13 @@
+package com.yulyschr.gym_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yulyschr/gym_api/GymApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulyschr.gym_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
